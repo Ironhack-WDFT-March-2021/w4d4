ecb200e98fde0028ed529b700e5e99d4a78f901a
Jan Redmann <janredmann1@gmail.com>
2021-04-15 12:48:06 +0200

    add layouts and partials

--- layouts-partials/app.js
+++ layouts-partials/app.js
@@ -1,23 +1,33 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 // import the movies from movies.js
 
 const movies = require('./movies.json');
 
-console.log(movies);
+// register the public folder
+app.use(express.static('public'));
+
+// these two lines are necessary to be able to use partials
+// https://www.npmjs.com/package/hbs - hbs docs
+const handlebars = require('hbs');
+handlebars.registerPartials(__dirname + '/views/partials');
+
+// console.log(movies);
 
 app.set('view engine', 'hbs');
 
+
+
 app.get('/', (req, res) => {
-    const todoList = ['learn node', 'learn react', 'play nintendo', 'watch devs', 'watch dark'];
-    const name = 'Thomas';
-    const upperCased = name.toUpperCase();
-    res.render('index', { name: upperCased, list: todoList, tag: '<h3>This is html</h1>' });
+    // add {layout: false} to the object if you don't want to use the layout file for this route
+    res.render('movies', { moviesList: movies });
 })
 
-app.get('/movies', (req, res) => {
-    res.render('movies', { moviesList: movies });
+app.get('/godfather', (req, res) => {
+    // find the movie godfather in the movies array
+    const godfather = movies.find((movie) => movie.title === 'The Godfather');
+    res.render('movieDetails', { clickedMovie: godfather });
 })
 
 app.listen(3000, () => {
